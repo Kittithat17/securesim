89b98de50a0db25a026af05f63b88692c998e2a1
Kittithat17 <hdhsdhdhdd1@gmail.com>
2026-03-02 01:12:42 +0700

    try

--- app/(admin)/campaigns/page.tsx
+++ app/(admin)/campaigns/page.tsx
@@ -6,7 +6,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
-
+export const dynamic = "force-dynamic";
 export default async function CampaignsPage() {
   const { data } = await supabase
     .from("campaigns")
