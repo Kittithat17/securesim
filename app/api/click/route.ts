d7bc6cb2df9262834a3bb66efbedc6616ceb9bd7
Kittithat17 <hdhsdhdhdd1@gmail.com>
2026-03-28 00:31:53 +0700

    sd

--- app/api/click/route.ts
+++ app/api/click/route.ts
@@ -2,13 +2,14 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
+
 
 export async function POST(req: Request) {
   const { token } = await req.json();
+  const supabase = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
 
   if (!token) {
     return NextResponse.json({ error: "No token" }, { status: 400 });
