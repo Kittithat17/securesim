591cac0fa1543f7709b14708ee00f7a5e5842bcd
Kittithat17 <hdhsdhdhdd1@gmail.com>
2026-03-28 00:23:00 +0700

    fix

--- app/(admin)/campaigns/[id]/page.tsx
+++ app/(admin)/campaigns/[id]/page.tsx
@@ -37,7 +37,7 @@ type CampaignTarget = {
 };
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
